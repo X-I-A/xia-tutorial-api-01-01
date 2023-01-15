637ae816dce12e4bc8ccc53a859576d65e24f36e
Soral <cx.soral@gmail.com>
2023-01-15 21:12:56 +0100

    feat: adding no required options

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,7 +1,16 @@
 from __future__ import annotations
-from xia_fields import StringField
-from xia_engine import Document, EmbeddedDocument, EmbeddedDocumentField
+from xia_fields import StringField, IntField
+from xia_engine import Document, EmbeddedDocument, EmbeddedDocumentField, ListField
 from xia_api import XiaActionResult
+
+
+class PurchaseOrderItem(EmbeddedDocument):
+    _key_fields = ["line_number"]
+
+    line_number = IntField(description="Order line number", sample=1)
+    item_id = StringField(description="Item ID", sample="UX00001")
+    item_name = StringField(description="Item Name", sample="UX00001")
+    quantity = IntField(description="Ordered Qty", sample=1)
 
 
 class Address(EmbeddedDocument):
@@ -12,12 +21,14 @@ class Address(EmbeddedDocument):
 
 class PurchaseOrder(Document):
     _actions = {"set_paid": {"out": XiaActionResult}}
+    _key_fields = ["po_number"]
 
     po_number: str = StringField(description="Purchase Order Number")
     order_status: str = StringField(description="Purchase Order Status",
                                     required=True,
                                     default="new",
                                     choices=["new", "paid", "delivered"])
+    items: list = ListField(EmbeddedDocumentField(document_type=PurchaseOrderItem))
     delivery_address = EmbeddedDocumentField(document_type=Address)
 
     def set_paid(self, _acl=None):
