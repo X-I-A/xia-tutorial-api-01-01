62549b52b4db416f8e033e27d468f7f7d9b6a7d7
Soral <cx.soral@gmail.com>
2023-01-15 19:11:28 +0100

    feat: new sample code version

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 from xia_fields import StringField
 from xia_engine import Document, EmbeddedDocument, EmbeddedDocumentField
+from xia_api import XiaActionResult
 
 
 class Address(EmbeddedDocument):
@@ -9,9 +11,19 @@ class Address(EmbeddedDocument):
 
 
 class PurchaseOrder(Document):
+    _actions = {"set_paid": {"out": XiaActionResult}}
+
     po_number: str = StringField(description="Purchase Order Number")
     order_status: str = StringField(description="Purchase Order Status",
                                     required=True,
                                     default="new",
                                     choices=["new", "paid", "delivered"])
     delivery_address = EmbeddedDocumentField(document_type=Address)
+
+    def set_paid(self):
+        self.update(order_status="paid")
+        self.reload()
+        if self.order_status == "paid":
+            return XiaActionResult(successful=True, message="Order status successfully changed")
+        else:
+            return XiaActionResult(successful=False, message="Something goes wrong")
